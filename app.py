3f26e0461583c7a59e3cef5c9f7c55ebac3b75fb
Sergio <sergio@example.com>
2026-09-25 19:40:43 -0300

    feat: implementar servidor Flask base y servir archivos estáticos (Bloque 1)

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='.', static_url_path='')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory('.', 'index.html')
+
+if __name__ == '__main__':
+    print("Iniciando Servidor LogiTrack Express (Fase 2)...")
+    app.run(host='0.0.0.0', port=5000, debug=True)
